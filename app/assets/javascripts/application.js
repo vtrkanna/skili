e90ed327bb16af8a8fc9ba1d91ddcc3bff887b4d
kanna <kannan@kreatio.com>
2016-01-02 17:55:27 +0530

    login changes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
+//= require jquery.min.js
+//= require bootstrap.min.js
+//= require jquery.flexslider.js
+//= require responsiveslides.min.js
+//= require underscore-min.js
+//= require moment-2.2.1.js
+//= require jquery.countdown.js
 //= require_tree .
